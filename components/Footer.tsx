70ad98f3b912efc470b61535cf6f7bc6c6e4c56e
Mazid <kmazid431@gmail.com>
2024-08-27 12:39:34 +0530

    🧹Get over with 🧸

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,29 +2,39 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Linkedin, Github } from "lucide-react";
 
 function Footer() {
   return (
-    <div className="flex items-center gap-10 py-16">
-      <Link
-        className="text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
-        href="https://www.linkedin.com/in/md-mazid-519942301/"
-      >
-        Linked!n &nbsp;&#129109;
-      </Link>
-      <Link
-        href="https://mazid-portfolio.vercel.app/"
-        className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
-      >
-        <span className="pr-2">Made by</span>
-        <span className="pl-1 font-medium text-slate-200">Mazid</span>
-      </Link>
-      <Link
-        href="https://github.com/mazid79"
-        className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
-      >
-        Github &nbsp;&#129109;
-      </Link>
+    <div className="text-white py-12">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 text-center md:text-left items-center">
+        {/* LinkedIn Link */}
+        <Link
+          className="flex items-center justify-center md:justify-start text-sm font-medium hover:text-gray-300 transition-all duration-500 ease-in-out"
+          href="https://www.linkedin.com/in/md-mazid-519942301/"
+        >
+          <Linkedin className="mr-2 w-5 h-5" />
+          Linked!n
+        </Link>
+
+        {/* Portfolio Link */}
+        <Link
+          href="https://mazid-portfolio.vercel.app/"
+          className="flex items-center justify-center md:justify-start text-sm font-medium hover:text-gray-300 transition-all duration-500 ease-in-out"
+        >
+          <span className="pr-2">Made by</span>
+          <span className="font-semibold text-white">Mazid</span>
+        </Link>
+
+        {/* GitHub Link */}
+        <Link
+          href="https://github.com/mazid79"
+          className="flex items-center justify-center md:justify-start text-sm font-medium hover:text-gray-300 transition-all duration-500 ease-in-out"
+        >
+          <Github className="mr-2 w-5 h-5" />
+          GitHub
+        </Link>
+      </div>
     </div>
   );
 }
